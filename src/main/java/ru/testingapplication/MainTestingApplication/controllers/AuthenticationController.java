023d55f874045a215ba13626ef872c7e918bed38
roman <romanshmidt12@mail.ru>
2023-05-25 16:08:03 +0300

    Добавил CorsConfig, добавление пользователя в контекст Spring Security после аутентификации

--- src/main/java/ru/testingapplication/MainTestingApplication/controllers/AuthenticationController.java
+++ src/main/java/ru/testingapplication/MainTestingApplication/controllers/AuthenticationController.java
@@ -3,6 +3,8 @@ package ru.testingapplication.MainTestingApplication.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,9 +30,10 @@ public class AuthenticationController {
     @PostMapping("/authenticate")
     public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) throws Exception {
         try {
-            authenticationManager.authenticate(
+            Authentication authentication = authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(authenticationRequest.getUsername(), authenticationRequest.getPassword())
             );
+            SecurityContextHolder.getContext().setAuthentication(authentication);
         } catch (Exception e) {
             throw new Exception("Incorrect username or password", e);
         }
